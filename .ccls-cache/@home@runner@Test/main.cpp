1a3587a46234c28be5f67fd1d73fdd66d2c9e56b
Javier Solis <>
2022-08-26 15:36:51 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@Test/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  //int choice;
+  //cout << "number ";
+  //cin >> choice; 
+  //cout << choice << endl;
+  cout << "2 + 5 = "<< 2+5 << endl;
+  cout << "2 / 5 = "<< 2/5 << endl;
+  cout << "2 / 5 = "<< 2.0/5 << endl;
+  cout << "!0 % 4 = "<< 10%4<< endl;
+  cout << "-10 % 4 ="<< -10%4<< endl;
+
+  cout << "2.37E-5 * 523E-8 ="<<2.37E-5 * 523E-8 <<endl;
+  cout << "3.5 /2 +1 = " << 3.5 /2 +1<< endl;
+  double a = 3.5;
+  int b = 2;
+  int c = 1;
+  int d;
+  d = a/b +c;
+  cout << d<< endl;
+}
